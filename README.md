87b46d7170139102d49202c5a75113f437fa4e0d
cristian16b <cristian.budzicz@gmail.com>
2021-07-26 16:41:45 -0300

    Se instalo tailwindcss

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
-Notas:
+Notas para instalar y configurar el proyecto:
 
-1) Ejecutar composer install
+1) Ejecutar 
+
+composer install
+
+2) Correr 
+
+npm install
+
+2) En el .env se ven los parametros de la bd. Los copio a continuacion:
+
+DB_DATABASE=laravel
+DB_USERNAME=root
+DB_PASSWORD=
+
+3) Instalar tailwind css ver la doc. oficial 
+https://tailwindcss.com/docs/guides/laravel
+
+4) 
+
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Laravel</title>
+
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -21,7 +23,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <!-- <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
@@ -127,6 +129,6 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </body>
 </html>
