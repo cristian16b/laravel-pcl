b225f715b3db972f1190e957e4a55aa564b7123c
cristian16b <cristian.budzicz@gmail.com>
2021-07-26 19:15:10 -0300

    Renombro dos columnas por errores de tipeo

--- database/migrations/2021_07_26_204219_create_usuarios_table.php
+++ database/migrations/2021_07_26_204219_create_usuarios_table.php
@@ -16,15 +16,16 @@ class CreateUsuariosTable extends Migration
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('nombree');
+            $table->string('nombre');
             $table->string('apellido');
             $table->string('correo');
             $table->string('clave');
             $table->string('grupo');
             $table->year('anio');
             // declaro la relacion usuario asignado a una empresa
-            $table->integer('empresa')->unsigned();
-            $table->index('empresa');
+            $table->integer('empresa_id')->unsigned();
+            $table->index('empresa_id');
+            // $table->foreign('empresa_id')->references('id')->on('empresa');
 
             // protected $fillable = ['nombre', 'apellido',
             // 'correo','clave','grupo','empresa','anio'];
