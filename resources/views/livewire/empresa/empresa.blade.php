890c34772997639654662526d1950ff54806f0bc
cristian16b <cristian.budzicz@gmail.com>
2021-07-26 22:07:34 -0300

    Se suben cambios parciales en el listado de empresas

--- resources/views/livewire/empresa/empresa.blade.php
+++ resources/views/livewire/empresa/empresa.blade.php
@@ -1,4 +1,41 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
-    empresa funciona
+<div>  
+    @if($updateMode)
+        @include('livewire.empresa.update')
+    @else
+        @include('livewire.empresa.create')
+    @endif
+<table class="table-auto w-full">
+        <thead>
+            <tr class="bg-gray-200">
+                <th class="px-4 py-2">#</th>
+                <th class="px-4 py-2">Nombre</th>
+                <th class="px-4 py-2">Descripción</th>
+                <th class="px-4 py-2">Dirección</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($data as $item)
+                <tr class="text-center">
+                    <td class="border px-4 py-2">{{ $item->id }}</td>
+                    <td class="border px-4 py-2">{{ $item->nombre }}</td>
+                    <td class="border px-4 py-2">{{ $item->descripcion }}</td>
+                    <td class="border px-4 py-2">{{ $item->direccion }}</td>
+                    <td class="border px-4 py-2">
+                        <button wire:click="edit({{ $item->id }})" 
+                            class="px-2 py-1 bg-blue-200 text-blue-500 hover:bg-blue-500 hover:text-white rounded">
+                            Editar
+                        </button>
+                        <button wire:click="destroy({{ $item->id }})" 
+                            class="px-2 py-1 bg-red-200 text-red-500 hover:bg-red-500 hover:text-white rounded">
+                            Borrar
+                        </button>
+                    </td>
+                </tr>
+            @empty
+                <tr class="text-center">
+                    <td colspan="4" class="py-3 italic">No hay información</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 </div>
