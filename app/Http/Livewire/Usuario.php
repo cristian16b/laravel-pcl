e3684af2e660c7ea021489ad70c5890863aefb40
cristian16b <cristian.budzicz@gmail.com>
2021-07-27 13:37:30 -0300

    Reemplzando variables

--- app/Http/Livewire/Usuario.php
+++ app/Http/Livewire/Usuario.php
@@ -7,7 +7,7 @@ use App\Models\Usuario as ModelsUsuario;
 
 class Usuario extends Component
 {
-    public $data, $nombre, $descripcion, $selected_id, $direccion;
+    public $data, $nombre, $apellido, $correo, $clave, $grupo, $empresa, $anio, $selected_id;
     public $update = false;
 
     public function render()
@@ -19,20 +19,20 @@ class Usuario extends Component
     private function resetInput()
     {
         $this->nombre = null;
-        $this->descripcion = null;
-        $this->direccion = null;
+        $this->apellido = null;
+        $this->correo = null;
     }
     public function store()
     {
         $this->validate([
             'nombre' => 'required|min:5',
-            'descripcion' => 'required',
-            'direccion' => 'required'
+            'apellido' => 'required',
+            'correo' => 'required'
         ]);
         ModelsUsuario::create([
             'nombre' => $this->nombre,
-            'descripcion' => $this->descripcion,
-            'direccion' => $this->direccion,
+            'apellido' => $this->apellido,
+            'correo' => $this->correo,
         ]);
         $this->resetInput();
         session()->flash('message', 'Contacto agregado.');
@@ -42,23 +42,23 @@ class Usuario extends Component
         $record = ModelsUsuario::findOrFail($id);
         $this->selected_id = $id;
         $this->nombre = $record->nombre;
-        $this->descripcion = $record->descripcion;
-        $this->direccion = $record->direccion;
+        $this->apellido = $record->apellido;
+        $this->correo = $record->correo;
         $this->update = true;
     }
     public function update()
     {
         $this->validate([
             'nombre' => 'required|min:5',
-            'descripcion' => 'required',
-            'direccion' => 'required'
+            'apellido' => 'required',
+            'correo' => 'required'
         ]);
         if ($this->selected_id) {
             $record = ModelsUsuario::find($this->selected_id);
             $record->update([
                 'nombre' => $this->nombre,
-                'descripcion' => $this->descripcion,
-                'direccion' => $this->direccion,
+                'apellido' => $this->apellido,
+                'correo' => $this->correo,
             ]);
             $this->resetInput();
             $this->update = false;
