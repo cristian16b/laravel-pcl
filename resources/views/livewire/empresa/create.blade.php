9f2a2ccf2124efe4721843e941b63f2eedf0a7ab
cristian16b <cristian.budzicz@gmail.com>
2021-07-28 14:53:50 -0300

    Reenombro un boton en el empresa crear

--- resources/views/livewire/empresa/create.blade.php
+++ resources/views/livewire/empresa/create.blade.php
@@ -26,7 +26,7 @@
         </div>
         <div class="w-auto pl-3 text-right">
             <div class="pt-5">
-                <button wire:click="store()" class="px-3 py-2 bg-purple-200 text-purple-500 hover:bg-purple-500 hover:text-purple-100 rounded">Agregar contacto</button>
+                <button wire:click="store()" class="px-3 py-2 bg-purple-200 text-purple-500 hover:bg-purple-500 hover:text-purple-100 rounded">Agregar empresa</button>
             </div>
         </div>
     </div>
